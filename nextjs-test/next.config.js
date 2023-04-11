37db49be7b7dc414764570d94cdb3e149c1082b9
Yifan Shi <stevenls1118@msn.com>
2023-04-10 21:29:06 -0600

    Setup nextjs-test with prisma on psql

---
+++ nextjs-test/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.75ll.com', 'example.com'],
+  },
+}
+
+module.exports = nextConfig
