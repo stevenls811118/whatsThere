1a2baa6b6a4f15ee5b865ef73a1a49d669bcf405
Yifan Shi <stevenls1118@msn.com>
2023-04-13 21:24:25 -0600

    Update tables in DB, get a api/users page setup to send json format data. working on nextauth.

--- nextjs-whatsthere/pages/api/users.js
+++ nextjs-whatsthere/pages/api/users.js
@@ -1,6 +1,8 @@
-import { getUsers } from ''
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
-  const users = await getUsers()
-  res.status(200).json(users)
+  const prisma = new PrismaClient()
+  const users = await prisma.users.findMany()
+  res.status(200).json(users);
 }
+
