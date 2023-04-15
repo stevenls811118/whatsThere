86284db1d42f408279fd2f87275d71b29757b8bd
Yifan Shi <stevenls1118@msn.com>
2023-04-15 00:17:23 -0600

    Create api pages, adjust .eslintrc.json to get rid off red swiggly line for import

--- nextjs-whatsthere/pages/api/destinations.js
+++ nextjs-whatsthere/pages/api/destinations.js
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient()
-  const users = await prisma.users.findMany()
-  res.status(200).json(users);
+  const destinations = await prisma.destinations.findMany()
+  res.status(200).json(destinations);
 }
--- nextjs-whatsthere/pages/api/trip_planners.js
+++ nextjs-whatsthere/pages/api/trip_planners.js
@@ -2,6 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
   const prisma = new PrismaClient()
-  const users = await prisma.users.findMany()
-  res.status(200).json(users);
+  const trips = await prisma.trip_planners.findMany()
+  res.status(200).json(trips);
 }
