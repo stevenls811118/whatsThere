973423555dfa1a97a8bc930332c190db28bbe5fe
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-15 21:28:25 -0600

    add clarifying comments to login and register, create features component

--- react-whatsthere/src/components/Welcome/features.jsx
+++ react-whatsthere/src/components/Welcome/features.jsx
@@ -1,4 +1,20 @@
 export default function Features() {
     return 
-    //this component will display the main features 
+    <div>
+        <article>
+            <img></img>
+            <p></p>
+        </article>
+
+        <article>
+            <img></img>
+            <p></p>
+        </article>
+
+        <article>
+            <img></img>
+            <p></p>
+        </article>
+    </div>
+    //this component will display the main features of our app
 }
--- react-whatsthere/src/components/Welcome/login.jsx
+++ react-whatsthere/src/components/Welcome/login.jsx
@@ -1,4 +1,6 @@
 export default function Login() {
     return 
     //this component will allow users to login
+    //this will likely just handle OAuth 
+    
 }
