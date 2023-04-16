580aefce6a657e04bfd100e0168b508e52e6f814
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-15 18:05:40 -0600

    Refactor Header with theme colors and new text

--- react-whatsthere/src/components/Header/Header.jsx
+++ react-whatsthere/src/components/Header/Header.jsx
@@ -4,8 +4,13 @@ import React from "react";
 const Header = () => {
 
   return (
-    <div className="p-6 font-bold font-mono text-5xl text-center">
-      <h1>WhatsThere?</h1>
+    <div>
+      <div className="bg-primary text-white p-6 font-bold font-mono text-5xl text-center">
+        <h1>WhatsThere?</h1>
+      </div>
+      <div className="bg-primary text-white text-base text-center font-mono italic pb-4">
+        <p>The Perfect Travel Companion!</p>
+      </div>
     </div>
   )
 };
