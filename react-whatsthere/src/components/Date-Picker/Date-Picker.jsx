5d3ed9d5f4346cd97f1dcbf645c1cea3aef59cc4
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-15 21:10:12 -0600

    Add Calender date range picker feature

--- react-whatsthere/src/components/Date-Picker/Date-Picker.jsx
+++ react-whatsthere/src/components/Date-Picker/Date-Picker.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-date-picker';
+import { useState } from "react"
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Calender, DateInput, DayCell, Month, DateRange } from 'react-date-range';
 
-export default function DatePickerCalender() {
-  const [startDate, setStartDate] = useState(new Date());
+export default function DatePicker() {
+  
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ]);
 
   return (
-    <DatePicker
-      value={startDate}
-      onChange={date => setStartDate(date)}
+    <DateRange
+      editableDateInputs={true}
+      onChange={item => setState([item.selection])}
+      moveRangeOnFirstSelection={false}
+      ranges={state}
     />
-  );
+  )
 };
