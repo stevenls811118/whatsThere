d91dc8979fca52eca7a61fa5c200bb1ecd89dfcc
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-15 21:21:10 -0600

    add comments describing how to use videoPlayer component

--- react-whatsthere/src/components/Welcome/videoPlayer.jsx
+++ react-whatsthere/src/components/Welcome/videoPlayer.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
-
+// This component takes a src prop, which should be the URL of the video file you want to play.
+//  It uses a videoRef to reference the <video> element,
+//   and two event handlers to handle play and pause button clicks.
 export default function VideoPlayer({ src }) {
   const videoRef = useRef(null);
 
@@ -16,8 +18,8 @@ export default function VideoPlayer({ src }) {
       <video
         src={src}
         ref={videoRef}
-        width="500"
-        height="300"
+        width="1000"
+        height="800"
         controls
       />
       <div>
