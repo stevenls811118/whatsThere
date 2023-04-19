b3af758f50977880c09a2fc053fb6fa3f1e0a7f0
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-18 18:05:25 -0600

    create components for login and logout using google login

--- react-whatsthere/src/components/Welcome/login.jsx
+++ react-whatsthere/src/components/Welcome/login.jsx
@@ -1,6 +1,27 @@
+import { GoogleLogin } from 'react-google-login' ;
+const clientId = "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com"
+
 export default function Login() {
-    return 
+    const onSuccess = (res) =>{
+        console.log("Login success! Current user: ", res.profileObj)
+    }
+    const onFailure = (res) => {
+        console.log("Login failed! res: ", res)
+    }
+    return (
+    <div id="signInButton">
+        <GoogleLogin
+        client_id ={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+        ></GoogleLogin>
+
+    </div>
+    )
     //this component will allow users to login
     //this will likely just handle OAuth 
-    
+
 }
---
+++ react-whatsthere/src/components/Welcome/logout.jsx
@@ -0,0 +1,19 @@
+import { GoogleLogout } from 'react-google-login' ;
+const clientId = "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com"
+
+export default function Logout() {
+    const onSuccess = (res) =>{
+        console.log("Logout success")
+    }
+    return (
+        <div id="signOutButton">
+            <GoogleLogout
+            client_id={clientId}
+            buttonText={"Logout"}
+            onLogoutSuccess={onSuccess}>
+
+            </GoogleLogout>
+
+        </div>
+    )
+}
