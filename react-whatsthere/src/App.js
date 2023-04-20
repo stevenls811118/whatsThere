93c0fe792f08e3252438ec03062ae6a7ae874590
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-20 17:16:05 -0600

    update google authentication, add sign out button,

--- react-whatsthere/src/App.js
+++ react-whatsthere/src/App.js
@@ -7,50 +7,68 @@ import DatePickerCalender from "./components/Date-Picker/Date-Picker";
 import { CssBaseline, Grid } from "@mui/material";
 import { getAttractions } from "./components/Map/getAttractions";
 
-//to be moved to homepage 
-import Login from "./components/Welcome/login";
+//to be moved to homepage
+import jwt_decode from "jwt-decode";
+// import Login from "./components/Welcome/login";
 
 export default function App() {
-
   const [attractions, setAttractions] = useState([]);
-  function handleCallbackResponses(response) {
-    
-  }
 
   useEffect(() => {
-    getAttractions()
-    .then(data => {
+    getAttractions().then((data) => {
       setAttractions(data);
       console.log(data);
-    })
-  }, [])
+    });
+  }, []);
 
+  //Login
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    // response.credential is an encoded jwt
+    let userObj = jwt_decode(response.credential);
+    setUser(userObj); //decoded jwt object
+    //If we have no user: show sign in button, else show sign out button
+    document.getElementById("signInDiv").hidden = true;
+  }
+  function handleSignOut() {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
   useEffect(() => {
     // global google
     google.accounts.id.initialize({
-      client_id: "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
-      callback: handleCallbackResponses,
+      client_id:
+        "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
     });
 
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      {theme: "outline", size: "large"}
-    )
-    }, [])
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+  
   return (
     <div className="bg-gray-300">
       <CssBaseline />
       <Grid container spacing={1.5} item xs={12}>
-        <Grid className="flex-col" item xs={12} md={4}>  
+        <Grid className="flex-col" item xs={12} md={4}>
           <Grid className="flex-col">
-            <Header  />
+            <Header />
           </Grid>
           <Grid>
             <Planner />
           </Grid>
-          {/* if not logged in, show login component, else show users name*/}
           <Grid>
-            <Login/>
+            <div>
+              <div id="signInDiv"></div>
+              {Object.keys(user).length != 0 && (
+                <div>
+                  <button onClick={(e) => handleSignOut(e)}>Sign out</button>
+                </div>
+              )}
+            </div>
           </Grid>
           <Grid className="flex-col">
             {/* <Console /> */}
@@ -62,5 +80,5 @@ export default function App() {
         </Grid>
       </Grid>
     </div>
-  )
-};
+  );
+}
--- react-whatsthere/src/components/Welcome/login.jsx
+++ react-whatsthere/src/components/Welcome/login.jsx
@@ -1,19 +1,47 @@
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-export default function Login() {
-  return (
-    <div id="signInDiv">
-      <GoogleOAuthProvider clientId="632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com">
-        <GoogleLogin
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-        ;
-      </GoogleOAuthProvider>
-    </div>
-  );
-  //this component will allow users to login
-}
+// // import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { useState, useEffect } from "react";
+// import jwt_decode from "jwt-decode";
+
+// export default function Login() {
+
+//   const [user, setUser] = useState({});
+
+//   function handleCallbackResponse(response) {
+//     // response.credential is an encoded jwt
+//     let userObj = jwt_decode(response.credential);
+//     setUser(userObj); //decoded jwt object
+//     //If we have no user: show sign in button, else show sign out button
+//     document.getElementById("signInDiv").hidden = true;
+//   }
+//   function handleSignOut() {
+//     setUser({});
+//     document.getElementById("signInDiv").hidden = false;
+//   }
+//   useEffect(() => {
+//     // global google
+//     google.accounts.id.initialize({
+//       client_id:
+//         "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
+//       callback: handleCallbackResponse,
+//     });
+
+//     google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+//       theme: "outline",
+//       size: "large",
+//     });
+//   }, []);
+//   return (
+//     // <div id="signInDiv">
+//     //   <GoogleOAuthProvider clientId="632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com"></GoogleOAuthProvider>
+//     // </div>
+//     <div>
+//       <div id="signInDiv"></div>
+//       {Object.keys(user).length != 0 && (
+//         <div>
+//           <button onClick={(e) => handleSignOut(e)}>Sign out</button>
+//         </div>
+//       )}
+//     </div>
+//   );
+//   //this component will allow users to login
+// }
