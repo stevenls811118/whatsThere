8d122927a56fa6b2b38e22a25f3cdbaa3742a90e
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-20 18:55:55 -0600

    Merge with attractions around

--- react-whatsthere/src/components/Map/getAttractions.js
+++ react-whatsthere/src/components/Map/getAttractions.js
@@ -1,32 +1,10 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const URL = 'https://travel-advisor.p.rapidapi.com/attractions/list-in-boundary';
-
-const options = {
-
-  params: {
-    tr_longitude: '-113.9828387',
-    tr_latitude: '51.2476578',
-    bl_longitude: '-114.2228387',
-    bl_latitude: '51.1076578',
-    min_rating: '4.2',
-    limit: '20',
-  },
-  headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_X_Rapid_API_Key,
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
-
-export const getAttractions = async () => {
-=======
 const API_key = process.env.REACT_APP_X_Rapid_API_Key;
 const URL =
   "https://travel-advisor.p.rapidapi.com/attractions/list-in-boundary";
 
 export const getAttractions = async (ne, sw) => {
->>>>>>> origin/attractions-around
   try {
     const {
       data: { data },
