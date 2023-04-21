df4327e0c4bae2a7e68933e2ed40c8c78ae0e878
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-20 18:01:17 -0600

    Create attraction data for seed file

---
+++ nextjs-whatsthere/prisma/data/attractions.js
@@ -0,0 +1,26 @@
+export const attractions = [
+  {
+    name: "Calgary Tower",
+    address: "101 9 Ave SE",
+    city: "Calgary",
+    rating: 4.5,
+    startTime: "10:00",
+    endTime: "18:00",
+  },
+  {
+    name: "Royal Beagle Pub",
+    address: "5308 17 Ave SW, Calgary, AB T3E 6S6",
+    city: "Calgary",
+    rating: 5,
+    startTime: "10:00",
+    endTime: "18:00",
+  },
+  {
+    name: "Crunchy House",
+    address: "20 Crowfoot Crescent NW #420, Calgary, AB T3G 3T2",
+    city: "Calgary",
+    rating: 3,
+    startTime: "10:00",
+    endTime: "18:00",
+  },
+]
