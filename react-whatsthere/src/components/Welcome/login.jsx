16aa15db6984cb3261608410f5a1e96c530e09d5
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-20 20:19:37 -0600

    create welcome[age route(WIP), utilize gogole one tap sign in instead of rendered button

--- react-whatsthere/src/components/Welcome/login.jsx
+++ react-whatsthere/src/components/Welcome/login.jsx
@@ -1,19 +1,45 @@
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-export default function Login() {
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+
+const Login = () => {
+  const [user, setUser] = useState({});
+
+  const handleCallbackResponse = (response) => {
+    // response.credential is an encoded jwt
+    const userObj = jwt_decode(response.credential);
+    setUser(userObj); // decoded jwt object
+  };
+
+  const handleSignOut = () => {
+    setUser({});
+    google.accounts.id.prompt();
+  };
+
+  useEffect(() => {
+      // global google
+      google.accounts.id.initialize({
+        client_id:
+          "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
+        callback: handleCallbackResponse,
+      });
+
+
+      // prompts users to login with usual accounts (oneTap login)
+      google.accounts.id.prompt();
+  }, []);
+
   return (
-    <div id="signInButton">
-      <GoogleOAuthProvider clientId="632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com">
-        <GoogleLogin
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-        ;
-      </GoogleOAuthProvider>
+    <div>
+      <head>
+        <script src="https://accounts.google.com/gsi/client" async></script>
+      </head>
+      {Object.keys(user).length !== 0 && (
+        <div>
+          <button onClick={handleSignOut}>Sign out</button>
+        </div>
+      )}
     </div>
   );
-  //this component will allow users to login
-}
+};
+
+export default Login;
