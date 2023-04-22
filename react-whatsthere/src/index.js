601d5366ec00e4b4c13097208b6ceed4c1966446
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-22 17:52:02 -0600

    fix issue where login would not show

--- react-whatsthere/src/index.js
+++ react-whatsthere/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./tailwind.css";
-import LandingPage from "./components/Welcome";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
