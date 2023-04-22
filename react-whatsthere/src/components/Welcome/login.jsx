4049a316948fd209fc7d161a856b9082465fabf2
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-22 13:11:21 -0600

    style signout button using tailwindCSS

--- react-whatsthere/src/components/Welcome/login.jsx
+++ react-whatsthere/src/components/Welcome/login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
       {Object.keys(user).length === 0 && <div id="signInDiv"></div>}
 
       {Object.keys(user).length !== 0 && (
-        <div id="signOutDiv">
+        <div class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           <button onClick={handleSignOut}>Sign out</button>
         </div>
       )}
