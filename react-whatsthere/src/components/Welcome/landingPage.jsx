d072a6d3655791c090635ad2caf686cd802663c1
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-22 18:39:13 -0600

    comment out landing page, causing errors with login component, make login strictly === rather than ==

--- react-whatsthere/src/components/Welcome/landingPage.jsx
+++ react-whatsthere/src/components/Welcome/landingPage.jsx
@@ -5,7 +5,7 @@
 // export default function LandingPage() {
 //   return (
 //     <div>
-//       <Login />
+//       {/* <Login /> */}
 //       <Features />
 //       <VideoPlayer />
 //     </div>
