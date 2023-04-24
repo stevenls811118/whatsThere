c35bd74c68e1e74dc713b4db64c7911cfb5a27bc
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-24 16:29:42 -0600

    Move Login logic to own component

--- react-whatsthere/src/App.js
+++ react-whatsthere/src/App.js
@@ -11,6 +11,7 @@ import Adding from "./components/Map/Adding-Attractions";
 import Alert from "./components/Map/Alert";
 import CreateList from "./components/Planner/CreateList";
 import UserInfo from "./components/Users/UserInfo";
+import Login from "./components/Users/Login";
 
 // Helpers / Hooks
 import { getAttractions } from "./components/Map/getAttractions";
@@ -69,61 +70,6 @@ export default function App() {
       });
   }, []);
 
-//Login Functions
-  
-  const handleCallbackResponse = (response) => {
-    // response.credential is an encoded jwt
-    const userObj = jwt_decode(response.credential);
-    setUser(userObj); // decoded jwt object
-    console.log(user)
-
-    const userData = {
-      email: userObj.email,
-      name: userObj.name,
-      };
-
-    const userPic ={
-      picture: userObj.picture,
-    }
-
-    setUserData(userData);
-    setUserPicture(userPic);
-    console.log(`UserData: ${userData}`)
-    setUserId(userObj.id);
-  };
-
-  const handleSignOut = () => {
-    setUser({});
-    setUserData({});
-    setUserPicture({});
-    google.accounts?.id.prompt();
-  };
-
-  
-  useEffect(() => {
-    // global google
-    if (Object.keys(user).length === 0) {
-      google.accounts?.id.initialize({
-        client_id:
-          "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
-        callback: handleCallbackResponse,
-        auto_select: false,
-      });
-    }
-
-    // prompts users to login with usual accounts (oneTap login)
-    if (Object.keys(user).length === 0) {
-      google.accounts?.id.prompt();
-    }
-  }, [user]);
-
-  //login communication to DB
-  useEffect(() => {
-    if (userData) {
-      axios.put("/api/users", userData);
-    }
-  }, [userData]);
-
   return (
     <main className="bg-gray-300">
       <CssBaseline />
@@ -135,17 +81,16 @@ export default function App() {
               userData={userData}
               userPicture={userPicture}
               />
-              {Object.keys(user).length !== 0 && (
-                <div>
-                  <button
-                    onClick={handleSignOut}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                  >
-                    Sign out
-                  </button>
-                </div>
-              )}
+              <Login
+                user={user}
+                setUser={setUser}
+                userData={userData}
+                setUserData={setUserData}
+                userPicture={userPicture}
+                setUserPicture={setUserPicture}
+                userId={userId}
+                setUserId={setUserId} 
+              />
           </div>
           <CreateList
             userId={userId}
---
+++ react-whatsthere/src/components/Users/Login.jsx
@@ -0,0 +1,77 @@
+import { useEffect } from "react";
+import jwt_decode from "jwt-decode"
+import axios from "axios";
+
+export default function Login({user, setUser, userData, setUserData, userPicture, setUserPicture, userId, setUserId}) {
+
+  const handleCallbackResponse = (response) => {
+    // response.credential is an encoded jwt
+    const userObj = jwt_decode(response.credential);
+    setUser(userObj); // decoded jwt object
+    console.log(user)
+
+    const userData = {
+      email: userObj.email,
+      name: userObj.name,
+    };
+
+    const userPic = {
+      picture: userObj.picture,
+    }
+
+    setUserData(userData);
+    setUserPicture(userPic);
+    console.log(`UserData: ${userData}`)
+    setUserId(userObj.id);
+  };
+
+  const handleSignOut = () => {
+    setUser({});
+    setUserData({});
+    setUserPicture({});
+    google.accounts?.id.prompt();
+  };
+
+  const handleLogin = () => {
+    google.accounts?.id.prompt();
+  };
+
+  useEffect(() => {
+    // global google
+    if (Object.keys(user).length === 0) {
+      google.accounts?.id.initialize({
+        client_id:
+          "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
+        callback: handleCallbackResponse,
+        auto_select: false,
+        cancel_on_tap_outside: true,
+      });
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (userData) {
+      axios.put("/api/users", userData);
+    }
+  }, [userData]);
+
+  return (
+    <div>
+      {Object.keys(user).length === 0 ? (
+        <button
+          onClick={handleLogin}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          >
+            Login with Google
+          </button>
+      ) : (
+        <button
+          onClick={handleSignOut}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Sign Out
+        </button>
+      )}
+    </div>
+  );
+}
