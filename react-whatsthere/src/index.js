bc1baf713c494f7a54c84d627ba3b6e3c747d228
Alexander <apvannug.lhl.webdev@gmail.com>
2023-04-24 16:04:29 -0600

    Move google maps api logic here

--- react-whatsthere/src/index.js
+++ react-whatsthere/src/index.js
@@ -1,11 +1,41 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./tailwind.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Load the Google Maps API using a callback function
+const loadGoogleMapsApi = (callback) => {
+  const googleMapsScript = document.createElement('script');
+  googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`;
+  googleMapsScript.defer = true;
+  googleMapsScript.async = true;
+  googleMapsScript.onerror = () => {
+    console.error('Error loading Google Maps API');
+  };
+  googleMapsScript.onload = callback;
+  document.body.appendChild(googleMapsScript);
+};
+
+// Add script tag for Google Sign-In API
+const googleSignInScript = document.createElement('script');
+googleSignInScript.src = 'https://accounts.google.com/gsi/client';
+googleSignInScript.async = true;
+googleSignInScript.defer = true;
+document.body.appendChild(googleSignInScript);
+
+// Wait for the Google Maps API and Google Sign-In API to load
+Promise.all([
+  new Promise((resolve) => {
+    loadGoogleMapsApi(resolve);
+  }),
+  new Promise((resolve) => {
+    googleSignInScript.onload = resolve;
+  }),
+]).then(() => {
+  // Render the app once the APIs have loaded
+  createRoot(root).render(<App />);
+}).catch((error) => {
+  console.error('Error loading Google APIs', error);
+});
