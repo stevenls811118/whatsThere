0e3b7f085b29eed03c5c5e091a5a573cf2ef38e8
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-24 13:39:39 -0600

    format app.js

--- react-whatsthere/src/App.js
+++ react-whatsthere/src/App.js
@@ -7,7 +7,7 @@ import { getAttractions } from "./components/Map/getAttractions";
 import axios from "axios";
 import Adding from "./components/Map/Adding-Attractions";
 import Alert from "./components/Map/Alert";
-import jwt_decode from "jwt-decode"
+import jwt_decode from "jwt-decode";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -60,50 +60,47 @@ export default function App() {
         console.log(err);
       });
   }, []);
+  //Login
 
-//Login Functions
-  
-const handleCallbackResponse = (response) => {
-  // response.credential is an encoded jwt
-  const userObj = jwt_decode(response.credential);
-  setUser(userObj); // decoded jwt object
+  //Login Functions
 
-  const userData = {
-    email: userObj.email,
-    name: userObj.name,
+  const handleCallbackResponse = (response) => {
+    // response.credential is an encoded jwt
+    const userObj = jwt_decode(response.credential);
+    setUser(userObj); // decoded jwt object
+
+    const userData = {
+      email: userObj.email,
+      name: userObj.name,
+    };
+    setUserData(userData);
   };
-  setUserData(userData);
-};
 
-const handleSignOut = () => {
-  setUser({});
-  google.accounts?.id.prompt();
-};
-
-//Login
-useEffect(() => {
-  // global google
-  google.accounts?.id.initialize({
-    client_id:
-      "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
-    callback: handleCallbackResponse,
-  });
-
-
-  // prompts users to login with usual accounts (oneTap login)
-  if (Object.keys(user)) {
+  const handleSignOut = () => {
+    setUser({});
     google.accounts?.id.prompt();
-  }
-}, []);
+  };
 
-//login communication to DB
-useEffect(() => {
-  if (userData) {
-    axios.put("/api/users", userData);
-  }
-}, [userData]);
+  useEffect(() => {
+    // global google
+    google.accounts?.id.initialize({
+      client_id:
+        "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
 
+    // prompts users to login with usual accounts (oneTap login)
+    if (Object.keys(user)) {
+      google.accounts?.id.prompt();
+    }
+  }, []);
 
+  //login communication to DB
+  useEffect(() => {
+    if (userData) {
+      axios.put("/api/users", userData);
+    }
+  }, [userData]);
 
   return (
     <main className="bg-gray-300">
@@ -111,19 +108,18 @@ useEffect(() => {
       <Grid container spacing={1.5} item xs={12}>
         <Grid className="flex-col" item xs={12} md={4}>
           <Header />
-
           <Planner items={items} setItems={setItems} />
-
           {Object.keys(user).length !== 0 && (
-  <div>
-    <button
-      onClick={handleSignOut}
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-    >
-      Sign out
-    </button>
-  </div>
-)}        </Grid>
+            <div>
+              <button
+                onClick={handleSignOut}
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              >
+                Sign out
+              </button>
+            </div>
+          )}
+        </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoords={setCoords}
