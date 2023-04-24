aaed742ce26132753ed247c99df795e71b97b70f
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-04-24 13:24:17 -0600

    implement login changes from branch fix/OAuth

--- react-whatsthere/src/App.js
+++ react-whatsthere/src/App.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import Map from "./components/Map/Map";
 import Header from "./components/Header/Header";
 import Planner from "./components/Planner/planner";
-import Login from "./components/Welcome/login";
 import { CssBaseline, Grid } from "@mui/material";
 import { getAttractions } from "./components/Map/getAttractions";
 import axios from "axios";
 import Adding from "./components/Map/Adding-Attractions";
 import Alert from "./components/Map/Alert";
+import jwt_decode from "jwt-decode"
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -18,6 +18,10 @@ export default function App() {
   const [attractionInfoShown, setAttractionInfoShown] = useState(false);
   const [display, setDisplay] = useState("invisible");
   const [data, setData] = useState();
+
+  //login states
+  const [user, setUser] = useState({});
+  const [userData, setUserData] = useState();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((e) => {
@@ -57,6 +61,50 @@ export default function App() {
       });
   }, []);
 
+//Login Functions
+  
+const handleCallbackResponse = (response) => {
+  // response.credential is an encoded jwt
+  const userObj = jwt_decode(response.credential);
+  setUser(userObj); // decoded jwt object
+
+  const userData = {
+    email: userObj.email,
+    name: userObj.name,
+  };
+  setUserData(userData);
+};
+
+const handleSignOut = () => {
+  setUser({});
+  google.accounts?.id.prompt();
+};
+
+//Login
+useEffect(() => {
+  // global google
+  google.accounts?.id.initialize({
+    client_id:
+      "632068121299-unggfu717fg5kklshvbmn1kl6s6nl9ue.apps.googleusercontent.com",
+    callback: handleCallbackResponse,
+  });
+
+
+  // prompts users to login with usual accounts (oneTap login)
+  if (Object.keys(user)) {
+    google.accounts?.id.prompt();
+  }
+}, []);
+
+//login communication to DB
+useEffect(() => {
+  if (userData) {
+    axios.put("/api/users", userData);
+  }
+}, [userData]);
+
+
+
   return (
     <main className="bg-gray-300">
       <CssBaseline />
@@ -66,8 +114,16 @@ export default function App() {
 
           <Planner items={items} setItems={setItems} />
 
-          <Login />
-        </Grid>
+          {Object.keys(user).length !== 0 && (
+  <div>
+    <button
+      onClick={handleSignOut}
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+    >
+      Sign out
+    </button>
+  </div>
+)}        </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoords={setCoords}
